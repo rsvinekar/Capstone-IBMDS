9c50709a831c6ec7a4b0b1e5ff061fcbcfb439bc
Rithvik Vinekar <rsvinekar@gmail.com>
2023-08-15 02:36:46 +0530

    Update spacex_dash_app.py
    task 4 partially completed - skel

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -71,6 +71,9 @@ def get_pie_chart(entered_site):
 
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
+              [Input(component_id='site-dropdown', component_property='value'), Input(component_id="payload-slider", component_property="value")])
+def get_scatter_plot(entered_site,payload):
 
 
 # Run the app
